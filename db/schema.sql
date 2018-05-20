1b6540160cd7b229c13858d5f4d0cfac3f253658
dbunadi <dbunadi@gmail.com>
2018-05-20 15:54:04 +1000

    added updates from original repo

--- db/schema.sql
+++ db/schema.sql
@@ -10,13 +10,13 @@ create table users(
 drop table if exists campaigns;
 create table campaigns(
     id integer primary key autoincrement,
-    name char(80) not null,
-    description text not null,
+    name string not null,
+    description string not null,
+    tags string not null,
     start_date string not null,
     end_date string not null,
     comments_target integer not null,
     comments_sentiment_score float not null,
-    likes_percentage float not null,
     likes_target integer not null
 );
 
@@ -24,4 +24,15 @@ drop table if exists user_campaigns;
 create table user_campaigns(
     user_id integer references user(id),
     campaign_id integer references campaign(id)
+);
+
+drop table if exists events;
+create table events(
+    id integer primary key autoincrement,
+    event_name string not null,
+    event_description string not null,
+    event_type string not null,
+    start_date string not null,
+    end_date sting not null,
+    campaign integer references campaign(id)
 );
